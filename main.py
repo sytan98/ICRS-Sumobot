ee3e4e00635b8d882939ccf631ac0c35f2203c83
Jia Qi Poon <poonjiaqi@gmail.com>
2019-12-05 19:16:11 +0000

    Update ultrasound.py to be OOP, update main to test OOP model of ultrasound.py

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 import time
-from infrared import infrared
+#from infrared import infrared
 from ultrasound import ultrasound
-from output import output
+#from output import output
 
-# Instantiate the Dog object
-ir = infrared(20, 15, 8)
-us = ultrasound(15, 8)
+def main():
+
+    # Instantiate ir and us objects. We will read from these objects
+    us = ultrasound()
+
+    # Pre-checks before starting main loop
+    assert us.sensor_is_online()
+
+    # Main loop to be run
+    try:
+        while True:
+            time.sleep(1.0)
+            print(us.read())
+    except KeyboardInterrupt:
+        print("Keyboard interrupt received")
+        # Do some graceful shutdown instead of immediately crashing the program
+
+
+if __name__ == '__main__':
+    main()
+
+
+"""
+ir = infrared(triggerpin=20,
+              uartpin=15,
+              numbers=8)
+us = ultrasound(uartpin=15,
+                numbers=8)
 
 test = ir.check()
 test2 = us.check()
 print(test)
-print(test2)
-
+print(test2)"""
