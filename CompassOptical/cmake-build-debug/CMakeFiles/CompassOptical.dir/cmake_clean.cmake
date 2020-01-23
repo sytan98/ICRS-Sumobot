57573447f75f6afac17954d43d608b974f8c3d7d
tianyilim <0.tianyi.lim@gmail.com>
2020-01-23 02:11:28 +0000

    Added maximum distance calculations for front and side ssensors. Not yet fully tested. Needs to be optimised for actual placement of each sensor on the physical robot.

---
+++ CompassOptical/cmake-build-debug/CMakeFiles/CompassOptical.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CompassOptical.dir/main.c.o"
+  "CompassOptical"
+  "CompassOptical.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CompassOptical.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
